5be54dc78d3b228b60af0fdc45d3a39865d15d89
thomaslautenb <t.lautenbacher@campus.tu-berlin.de>
2024-07-01 14:33:12 +0200

    update bash

--- scripts/runjob.sh
+++ scripts/runjob.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 #SBATCH --job-name="test_code_grid2op"
-#SBATCH --time=00:05:00
+#SBATCH --time=00:10:00
+#SBATCH --partition=gpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --partition=compute
+#SBATCH --gpus-per-task=1
 #SBATCH --mem-per-cpu=1GB
 #SBATCH --account=research-eemcs-ese
 
 # Load modules:
 module load 2023r1
 module load openmpi
-module load miniconda3 py-pip
+module load miniconda3 
+module load py-pip
 
 # Set conda env:
 unset CONDA_SHLVL
@@ -20,6 +22,6 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 # Activate conda, run job, deactivate conda
 conda activate top
 
-srun MORL_execution.py > morl.log
+srun python MORL_execution.py > morl.log
 
 conda deactivate
